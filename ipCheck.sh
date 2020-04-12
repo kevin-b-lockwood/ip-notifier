e3f00effc58250fb7424f9875cceae4849ed3778
Kevin Lockwood <kevin-b-lockwood@github.com>
2020-04-12 23:38:07 +0000

    Adjusted regex to work with typical IP addresses

--- ipCheck.sh
+++ ipCheck.sh
@@ -3,10 +3,10 @@
 INTERFACE='enp3s0'
 LASTIPFILE='/home/diogenes/ip-notifier/.last_ip_addr';
 TIME=`date`;
-ADDRESS='agentkbl@gmail.com'
+ADDRESS='agentkbl@gmail.com lgantar19@georgefox.edu'
 
 LASTIP=`cat ${LASTIPFILE}`;
-MYIP=`/sbin/ifconfig enp3s0 | grep -Eo 10.55.66.[0-9]\{1,3\}`;
+MYIP=`/sbin/ifconfig enp3s0 | grep -Eo [0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,3\} | head -1`;
 
 echo ${MYIP}
 if [[ ${MYIP} != ${LASTIP} ]]
